89ca99ea832349909b71e1d1ca0aacd1e65359ea
Rodrigonmt <rodrigo372.nmt@gmail.com>
2025-03-30 11:20:39 -0300

    antes merge

--- obj/Debug/net8.0-windows/Academia01.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Academia01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Academia01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e6988eb39ea8e4044fbd5e479b7d9bdfac376c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+071b22dcb11cd8d99cd7d060a2b2d59639d80dc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Academia01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Academia01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
